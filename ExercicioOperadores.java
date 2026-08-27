b17abb17ce71507e04ce9a6cc82f1e99597f7df3
Fabiany Souza/Dev <fabianymariapadilhadesouza165@gmail.com>
2026-08-26 21:17:39 -0400

    Adiciona exercício de operadores em Java

---
+++ ExercicioOperadores.java
@@ -0,0 +1,14 @@
+public class ExercicioOperadores {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 4;
+
+        System.out.println("Soma: " + (a + b));
+        System.out.println("Subtração: " + (a - b));
+        System.out.println("Multiplicação: " + (a * b));
+        System.out.println("Divisão: " + (a / b));
+        System.out.println("Resto: " + (a % b));
+    }
+}
